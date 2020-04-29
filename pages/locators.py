111e4da88bc18bbbb2b54ca8af26bd947f75b97a
scorpion-kit <nezalyma@gmail.com>
2020-04-30 00:26:12 +0300

    add variables for product page add 2 methods is_not_element_present and is_disappeared

--- pages/locators.py
+++ pages/locators.py
@@ -11,3 +11,11 @@ class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#register_form")
     #url
     LOGIN_URL = (By.CSS_SELECTOR, "#login_link")
+
+class ProductPageLocators():
+    ADD_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    BOOK_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    BOOK_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main .price_color")
+    BOOK_NAME_BASKET = (By.CSS_SELECTOR, "#messages>div:nth-child(1) strong")
+    BOOK_PRICE_BASKET = (By.CSS_SELECTOR, "#messages>div:nth-child(3) strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages>div:nth-child(1)")
