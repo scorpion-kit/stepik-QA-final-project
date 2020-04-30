1d7ba782d90f4863365f6b52629060cb8ee60838
scorpion-kit <nezalyma@gmail.com>
2020-04-30 13:45:38 +0300

    add variables for BasketPage and registration form

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,13 @@ from selenium.webdriver.common.by import By
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-group .btn.btn-default")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
+class BasketPageLocators():
+    BASKET_LINK = (By.CSS_SELECTOR, ".btn-group a.btn-default")
+    EMPTY_BASKET = (By.CSS_SELECTOR, ".basket-items")
+    EMPTY_BASKET_MESSAGE = (By.CSS_SELECTOR, "#content_inner p")
 
 class LoginPageLocators():
     #регистрация
@@ -12,6 +19,11 @@ class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#register_form")
     #url
     LOGIN_URL = (By.CSS_SELECTOR, "#login_link")
+
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD_1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_PASSWORD_2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "button[name='registration_submit']")
     
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
